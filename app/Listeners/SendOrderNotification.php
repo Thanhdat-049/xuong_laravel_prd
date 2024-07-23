096a5bfc52d8a6f08c946239a88d4d5e7241589e
Thanhdat-049 <l.t.dat04092004@gmail.com>
2024-07-23 10:01:25 +0700

    4 commit

--- app/Listeners/SendOrderNotification.php
+++ app/Listeners/SendOrderNotification.php
@@ -3,6 +3,8 @@
 namespace App\Listeners;
 
 use App\Events\OrderCreated;
+use App\Models\User;
+use App\Notifications\OrderNotication;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Log;
@@ -24,10 +26,8 @@ class SendOrderNotification implements ShouldQueue
     public function handle(OrderCreated $event): void
     {
         $order = $event->validatedData;
-        Mail::send('orders.mail_order', $order, function ($message) {
-            $message->to('l.t.dat04092004@gmail.com', 'Tutorials Point')
-                ->subject('Laravel Basic Testing Mail');
-        });
-        // Log::debug("Basic Email Sent. Check your inbox.");
+        $user = User::find(1); 
+        $user->notify(new OrderNotication($event->validatedData));
+
     }
 }
